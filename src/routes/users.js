8f268657ba8f70acd7219229a57021ae6a6e979c
speardragon <rkdckdfyyd@naver.com>
2022-07-21 17:36:34 +0900

    [#26] feat: Login API

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,13 +1,14 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 const user = require("../controller/users");
 const jwtMiddleware = require("../../config/jwtMiddleware");
 
-router.get('/user', user.userTest);
+router.get("/user", user.userTest);
 
+router.post("/user/signup", user.signup);
 
 // TODO : 회원가입 & 로그인 기능 완료되면 수정
 // 사용자가 예매한 티켓 정보 열람 API
-router.get('/user/ticket/reservation/info',user.getReserveInfo);
+router.get("/user/ticket/reservation/info", user.getReserveInfo);
 
 module.exports = router;
