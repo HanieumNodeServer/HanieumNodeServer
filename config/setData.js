ae8027608ad06acca656ce1b4b19d1774f573b83
jknadan <jknadan@gmail.com>
2022-08-10 12:01:06 +0900

    Batch add

--- config/setData.js
+++ config/setData.js
@@ -7,31 +7,29 @@ moment.tz.setDefault("Asia/Seoul");
 init();
 
 function init(){
+    getSchedule();
 
-    const init = new Promise((resolve, reject) => {
-        let temp = getTerminalIdList();
-
-        resolve(temp);
-
-        reject(new Error("실패"));
-    })
-
-    init.then( temp => {
-        // console.log(temp);
-        //getRouteData(temp);
-        getSchedule();
-    }).catch(error =>{
-        console.log(error);
-    })
+    // const init = new Promise((resolve, reject) => {
+    //     let temp = getTerminalIdList();
+    //
+    //     resolve(temp);
+    // })
+    //
+    // init.then( temp => {
+    //     getSchedule();
+    // }).catch(error =>{
+    //     console.log(error);
+    // })
 
 }
 
 async function getTerminalIdList () {
 
-    const sql = `select tmoneyTerId,terminalName from terminal;`;
+    const sql = `select tmoneyTerId,terminalName from TERMINAL;`;
     const connection = await pool.getConnection((conn)=>conn);
     let result = await connection.query(sql);
     connection.release();
+    // console.log(result)
     return result;
 
 }
@@ -57,7 +55,7 @@ async function getRouteData (temp) {
             let departureTerName = temp[0][i].terminalName;
             let departureTerID = temp[0][i].tmoneyTerId
             let arrivalTerList = result.data.response.TER_LIST;
-            console.log(i + " 번째 입력 중.....")
+            // console.log(i + " 번째 입력 중.....")
             // console.log(arrivalTerList)
             for(let j in arrivalTerList){
                 connection.query(sql,[departureTerID,arrivalTerList[j].TER_COD,departureTerName,arrivalTerList[j].TER_NAM]);
@@ -74,69 +72,40 @@ async function getRouteData (temp) {
 async function getSchedule(){
     let dateArray = [];
 
-    dateArray[0] = moment().add(30,"days").format("YYYYMMDD");
-
+    for(let i=0; i<31;i++){
+        dateArray[i] = moment().add(i,"days").format("YYYYMMDD");
+    }
 
     // console.log(dateArray);
-    const promises = [];
+    let promises = [];
 
     const connection = await pool.getConnection((conn)=>conn);
 
-    let terminalId = await connection.query(`select routeId,departTerId,arrivalTerId from route;`);
+    let terminalId = await connection.query(`select routeId,departTerId,arrivalTerId from ROUTE;`);
 
     let sql = `insert into 
-    schedule(routeId, allocateDate, corName, time, rotId, rotSqno, busGrade, alcnSqno, durationTime) 
+    SCHEDULE(routeId, allocateDate, corName, time, rotId, rotSqno, busGrade, alcnSqno, durationTime) 
         values  (?,?,?,?,?,?,?,?,?);`
 
     // console.log(terminalId[0]);
-
+    console.log(dateArray)
 
     for(let i in dateArray){ // 31
         promises.push(insertSchedule(i, terminalId, dateArray, connection, sql));
-
-/*        for(let i in terminalId[0]){ // 12480
-            let url = 'https://apigw.tmoney.co.kr:5556/gateway/xzzIbtListGet/v1/ibt_list/' +
-                dateArray[j] + '/' +
-                '0000' + '/' +
-                terminalId[0][i].departTerId + '/' +
-                terminalId[0][i].arrivalTerId + '/' +
-                '9' + '/' +
-                '0';
-
-            let result = await axios.get(url,{headers:{"x-Gateway-APIKey": "42e5892b-0e48-4b0b-8cdc-6b9bc8699bc1"}})
-                .then((result)=>{
-                    console.log(i +' 의 '+ j +" 번째");
-                    // console.log(result.data.response);
-                    return result.data.response;
-                })
-
-            if(result === null){
-                break;
-            }else{
-                for(let k in result.LINE_LIST){ // 20
-                    await connection.query(sql,[terminalId[0][i].routeId,dateArray[j],
-                        result.LINE_LIST[k].COR_NAM,result.LINE_LIST[k].TIM_TIM_O,
-                    result.LINE_LIST[k].ROT_ID,result.LINE_LIST[k].ROT_SQNO,
-                    result.LINE_LIST[k].BUS_GRA_O,result.LINE_LIST[k].ALCN_SQNO,
-                    result.LINE_LIST[k].LIN_TIM])
-                    console.log(k + " 번째 입력");
-                }
-            }
-
-        }*/
-
     }
 
+//    console.log(promises);
+
     Promise.allSettled(promises)
-        .then(() => {
-            console.log('completed');
+        .then((result) => {
+            console.log(result);
         })
 }
 
-const insertSchedule = async function (i, terminalId, dateArray, connection, sql) {
+let insertSchedule = async function (i, terminalId, dateArray, connection, sql) {
 
     for(let j in terminalId[0]){ // 31
-        // console.log(terminalId[0])
+
         let url = 'https://apigw.tmoney.co.kr:5556/gateway/xzzIbtListGet/v1/ibt_list/' +
             dateArray[i] + '/' +
             '0000' + '/' +
@@ -145,21 +114,19 @@ const insertSchedule = async function (i, terminalId, dateArray, connection, sql
             '9' + '/' +
             '0';
 
-        // const promises2 = [];
-
-        // promises2.push(axios.get(url,{headers:{"x-Gateway-APIKey": "42e5892b-0e48-4b0b-8cdc-6b9bc8699bc1"}}));
+        // console.log(url);
 
         let result = await axios.get(url,{headers:{"x-Gateway-APIKey": "42e5892b-0e48-4b0b-8cdc-6b9bc8699bc1"}})
             .then((result)=>{
 
                 console.log(i +' 의 '+ j +" 번째");
-
                 return result.data.response;
             })
 
-        // Promise.all(promises2).then
+        // console.log(result);
 
-        if(result !== null){
+        if(result){
+
             for(let k in result.LINE_LIST){ // 20
                 await connection.query(sql,[terminalId[0][j].routeId,dateArray[i],
                     result.LINE_LIST[k].COR_NAM,result.LINE_LIST[k].TIM_TIM_O,
@@ -167,6 +134,7 @@ const insertSchedule = async function (i, terminalId, dateArray, connection, sql
                     result.LINE_LIST[k].BUS_GRA_O,result.LINE_LIST[k].ALCN_SQNO,
                     result.LINE_LIST[k].LIN_TIM])
 
+                //console.log(resultRow);
             }
         }
 
